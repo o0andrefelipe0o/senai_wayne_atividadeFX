e4c4c35a57ba45db4fa039952445a968e7f64042
André Felipe <128185986+o0andrefelipe0o@users.noreply.github.com>
2025-07-02 18:34:16 -0300

    Add files via upload

--- db.sql
+++ db.sql
@@ -2,10 +2,14 @@ CREATE DATABASE IF NOT EXISTS wayne_enterprises_db;
 
 USE wayne_enterprises_db;
 
-CREATE TABLE IF NOT EXISTS funcionarios (
+CREATE TABLE IF NOT EXISTS usuario (
     id INT AUTO_INCREMENT PRIMARY KEY,
     usuario VARCHAR(100),
     senha VARCHAR(100),
+);
+
+CREATE TABLE IF NOT EXISTS funcionarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nome_completo VARCHAR(255) NOT NULL,
     cpf VARCHAR(14) NOT NULL UNIQUE,
     cargo VARCHAR(100),
@@ -15,7 +19,7 @@ CREATE TABLE IF NOT EXISTS funcionarios (
     data_nascimento DATE NOT NULL
 );
 
-CREATE TABLE ferias (
+CREATE TABLE IF NOT EXISTS ferias (
     id INT PRIMARY KEY AUTO_INCREMENT,
     data_inicio DATE NOT NULL,
     data_fim DATE NOT NULL,
